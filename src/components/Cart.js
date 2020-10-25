feaa87268b5a2520c7651e28a21aead085b68528
bgallday <you@example.com>
2020-10-25 12:21:57 -0700

    add-redux-cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import formatCurrency from "../util";
 import Fade from "react-reveal/Fade";
+import { connect } from "react-redux";
+import { removeFromCart } from "../actions/cartActions";
 
-export default class Cart extends Component {
+class Cart extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,7 +14,6 @@ export default class Cart extends Component {
       showCheckout: false,
     };
   }
-
   handleInput = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
@@ -23,7 +24,6 @@ export default class Cart extends Component {
       email: this.state.email,
       address: this.state.address,
       cartItems: this.props.cartItems,
-      total: this.props.cartItems.reduce((a, c) => a + c.price * c.count, 0),
     };
     this.props.createOrder(order);
   };
@@ -35,54 +35,55 @@ export default class Cart extends Component {
           <div className="cart cart-header">Cart is empty</div>
         ) : (
           <div className="cart cart-header">
-            You have {cartItems.length} items in the cart{" "}
+            You have {cartItems.length} in the cart{" "}
           </div>
         )}
-        <div className="cart">
-          <Fade left cascade>
-            <ul className="cart-items">
-              {cartItems.map((item) => (
-                <li key={item._id}>
-                  <div>
-                    <img src={item.image} alt={item.title}></img>
-                  </div>
-                  <div>
-                    <div>{item.title}</div>
-                    <div className="right">
-                      {formatCurrency(item.price)} x {item.count}{" "}
-                      <button
-                        className="button"
-                        onClick={() => this.props.removeFromCart(item)}
-                      >
-                        Remove
-                      </button>
+        <div>
+          <div className="cart">
+            <Fade left cascade>
+              <ul className="cart-items">
+                {cartItems.map((item) => (
+                  <li key={item._id}>
+                    <div>
+                      <img src={item.image} alt={item.title}></img>
                     </div>
+                    <div>
+                      <div>{item.title}</div>
+                      <div className="right">
+                        {formatCurrency(item.price)} x {item.count}{" "}
+                        <button
+                          className="button"
+                          onClick={() => this.props.removeFromCart(item)}
+                        >
+                          Remove
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </Fade>
+          </div>
+          {cartItems.length !== 0 && (
+            <div>
+              <div className="cart">
+                <div className="total">
+                  <div>
+                    Total:{" "}
+                    {formatCurrency(
+                      cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                    )}
                   </div>
-                </li>
-              ))}
-            </ul>
-          </Fade>
-        </div>
-        {cartItems.length !== 0 && (
-          <div>
-            <div className="cart">
-              <div className="total">
-                <div>
-                  Total:{" "}
-                  {formatCurrency(
-                    cartItems.reduce((a, c) => a + c.price * c.count, 0)
-                  )}
+                  <button
+                    onClick={() => {
+                      this.setState({ showCheckout: true });
+                    }}
+                    className="button primary"
+                  >
+                    Proceed
+                  </button>
                 </div>
-                <button
-                  onClick={() => {
-                    this.setState({ showCheckout: true });
-                  }}
-                  className="button primary"
-                >
-                  Proceed
-                </button>
               </div>
-
               {this.state.showCheckout && (
                 <Fade right cascade>
                   <div className="cart">
@@ -126,9 +127,16 @@ export default class Cart extends Component {
                 </Fade>
               )}
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     );
   }
 }
+
+export default connect(
+  (state) => ({
+    cartItems: state.cart.cartItems,
+  }),
+  { removeFromCart }
+)(Cart);
